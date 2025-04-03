5adcd568adbaf561ac91cc96ddfb1e8d612323d6
AmberIsCoding <52209410+ambercaravalho@users.noreply.github.com>
2025-04-02 21:53:05 -0700

    revise bash colors for better contrast

--- rm-background-manager/wallpaper-manager.sh
+++ rm-background-manager/wallpaper-manager.sh
@@ -3,8 +3,8 @@
 # Color and formatting definitions
 BOLD="\033[1m"
 GREEN="\033[0;32m"
-BLUE="\033[0;34m"
-YELLOW="\033[0;33m"
+BLUE="\033[0;96m"
+YELLOW="\033[0;93m"
 RED="\033[0;31m"
 RESET="\033[0m"
 SEPARATOR="━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
